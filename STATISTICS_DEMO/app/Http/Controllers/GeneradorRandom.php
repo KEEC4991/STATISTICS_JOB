c1cf3bd57723b7af0d6d6892e7a8021fcebb3e6a
KEEC4991 <42172933+KEEC4991@users.noreply.github.com>
2019-03-08 15:54:49 -0600

    Update de controladores

--- STATISTICS_DEMO/app/Http/Controllers/GeneradorRandom.php
+++ STATISTICS_DEMO/app/Http/Controllers/GeneradorRandom.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class GeneradorRandom extends Controller
 {
-    //
+    public function index(){
+       	//Generacion del numero random
+        $rand = rand(0,100);
+
+        //Fecha del servidor, cuando se realizo la solicitud
+        $fecha_Hoy = date("Y-m-d H:i:s");
+
+        //Json Object de respuesta
+        $respuesta = [ 'valor' => $rand, 'fecha_generacion' => $fecha_Hoy];
+
+        //Almacenamiento del valor
+        //Headers para el response
+        $headers = [
+                'Access-Control-Allow-Origin'      => '*',
+                'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+                'Access-Control-Allow-Credentials' => 'true',
+                'Access-Control-Max-Age'           => '86400',
+                'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+            ];
+
+        //Retorno del valor de respuesta
+        return response()->json($respuesta,200,$headers);
+    }
 }
--- STATISTICS_DEMO/routes/web.php
+++ STATISTICS_DEMO/routes/web.php
@@ -21,33 +21,7 @@ Route::get('/reporte',function(){
     return view('reports');
 });
 
-Route::get('/generacion_random', function(){
-
-	//Generacion del numero random
-    $rand = rand(0,100);
-
-    //Fecha del servidor, cuando se realizo la solicitud
-    $fecha_Hoy = date("Y-m-d H:i:s");
-
-    //Json Object de respuesta
-    $respuesta = [ 'valor' => $rand, 'fecha_generacion' => $fecha_Hoy];
-
-    //Almacenamiento del valor
-/*
-*/
-    //Headers para el response
-    $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
-        ];
-
-    //Retorno del valor de respuesta
-    return response()->json($respuesta,200,$headers);
-
-});
+Route::get('/generacion_random', 'GeneradorRandom@index');
 
 Route::get('/reporte_registros', function(){
 
