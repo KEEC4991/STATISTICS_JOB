a00b46e76bd6940852a98d44263216d1c9ac743a
KEEC4991 <42172933+KEEC4991@users.noreply.github.com>
2019-03-08 16:11:34 -0600

    Update de controladores

--- STATISTICS_DEMO/app/Http/Controllers/Reportes.php
+++ STATISTICS_DEMO/app/Http/Controllers/Reportes.php
@@ -10,10 +10,11 @@ class Reportes extends Controller
             
         $rand = rand(0,100);
         $fecha_Hoy = date("Y-m-d H:i:s");
-        $registro = new \App\RegistroRND;
-        $registro->Valor = $rand;
-        $registro->Fecha = $fecha_Hoy;
-        $registro->save();
+
+        $registro = \App\Registro::create(
+                ['Valor' => $rand,
+                'Fecha'  => $fecha_Hoy  
+            ]);
 
         $items = \App\Registro::all(['Valor', 'Fecha']);
         return $items; 
