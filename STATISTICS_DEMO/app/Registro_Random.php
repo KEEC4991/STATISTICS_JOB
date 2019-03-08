4804c747d4ae19b440a4ed993ec883d12d980434
KEEC4991 <keec.4991@gmail.com>
2019-03-08 14:33:37 -0600

    Update de modelos

--- STATISTICS_DEMO/app/Registro_Random.php
+++ STATISTICS_DEMO/app/Registro_Random.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Registro_Random extends Model
 {
-    //
+    protected $table = 'Registro_Aleatorios';
+
+    protected $attributes = [
+        'Id',
+        'Valor',
+        'Fecha'
+    ];
+
+    public function up(){
+        Schema::create('Registro_Aleatorios', function (Blueprint $table) {
+            $table->increments('Id');
+            $table->integer('Valor');
+            $table->string('Fecha');
+        });
+    }
+
+    public function down(){
+        Schema::dropIfExists('Registro');
+    }
 }
