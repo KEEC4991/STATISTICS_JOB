8bebf77b59695f48612485d527e5605f2245149c
KEEC4991 <keec.4991@gmail.com>
2019-03-08 14:07:46 -0600

    Update de consulta a base de datos, cambios por integracion a GoogleCloud

--- STATISTICS_DEMO/app/Registro.php
+++ STATISTICS_DEMO/app/Registro.php
@@ -18,7 +18,7 @@ class Registro extends Model
     ];
 
     public function up(){
-        Schema::create('Registros', function (Blueprint $table) {
+        Schema::create('Registro', function (Blueprint $table) {
             $table->increments('Id');
             $table->integer('Valor');
             $table->string('Fecha');
@@ -26,7 +26,7 @@ class Registro extends Model
     }
 
     public function down(){
-        Schema::dropIfExists('Registros');
+        Schema::dropIfExists('Registro');
     }
 
 }
